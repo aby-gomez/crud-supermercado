9e3822b1b06cdfc210a8116bfc26c322642ed86a
aby <topsytrabajo@gmail.com>
2025-12-08 00:47:34 -0300

    funcion abrir modal ok

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import CRUDView from './CRUDView'
+import 'flowbite';
 
 function App() {
   
 
   return (
     <>
-      <div className='flex justify-start items-start gap-1'>
+      <div className='flex justify-start items-start gap-1 '>
        
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
@@ -17,7 +18,7 @@ function App() {
         <h1 className='p-6'>CRUD SUPERMERCADO</h1>
       </div>
     
-    
+    <CRUDView></CRUDView>
     </>
   )
 }
